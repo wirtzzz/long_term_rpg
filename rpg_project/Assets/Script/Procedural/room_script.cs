cebe0612fdee8177d321969bedddc3efb3ca2392
wirtzzz <pauperandreas@gmail.com>
2020-05-22 18:29:39 +0200

    procedural : generation prototype

---
+++ rpg_project/Assets/Script/Procedural/room_script.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class room_script : MonoBehaviour
+{
+    public doorway[] doorWays;
+    public MeshCollider mesh_collider;
+    public enum RoomType{
+        start,
+        end,
+        corridor
+    };
+    public RoomType m_room_type;
+    public Bounds room_bounds {
+        get {return mesh_collider.bounds;}
+    }
+}
