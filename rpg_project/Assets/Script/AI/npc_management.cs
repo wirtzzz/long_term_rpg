474d07a8d4d0b4eeb6563f8e7557b3124b0db595
wirtzzz <pauperandreas@gmail.com>
2020-11-27 19:17:04 +0100

    added outline

--- rpg_project/Assets/Script/AI/npc_management.cs
+++ rpg_project/Assets/Script/AI/npc_management.cs
@@ -5,27 +5,34 @@ using UnityEngine.AI;
 
 public class npc_management : MonoBehaviour
 {
+    public enum NPCBehaviour
+    {
+        Movable,
+        Static
+    }
+    public NPCBehaviour behaviour;
     public GameObject storypoints_parent;
     public Vector3 cur_destination;
     private story_point[] destinations;
     private int cur_dest_index = 0;
     private GameObject cur_point;
+    private Outline outline_script;
     public NavMeshAgent agent;
     void Start()
     {
-        destinations = storypoints_parent.GetComponentsInChildren<story_point>(true);
-        NextStoryPoint();
+        outline_script = GetComponent<Outline>();
+        outline_script.enabled = false;
+        if(behaviour == NPCBehaviour.Movable)
+        {
+            if(storypoints_parent != null)
+            {
+                destinations = storypoints_parent.GetComponentsInChildren<story_point>(true);
+                NextStoryPoint();
+            }
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Vector3.Distance(agent.transform.position, agent.destination) <= 1.0f)
-        {
-            cur_point.SetActive(false);
-            NextStoryPoint();
-        }
-    }
     private void NextStoryPoint()
     {
         destinations[cur_dest_index].gameObject.SetActive(true);
@@ -37,5 +44,23 @@ public class npc_management : MonoBehaviour
             cur_dest_index = 0;
         else
             cur_dest_index++;
+        StartCoroutine(check_distance());
+    }
+    private void OnMouseOver()
+    {
+        outline_script.enabled = true;
+    }
+    private void OnMouseExit()
+    {
+        outline_script.enabled = false;
+    }
+    private IEnumerator check_distance()
+    {
+        while (Vector3.Distance(agent.transform.position, agent.destination) > 1.0f)
+        {
+            yield return null;
+        }
+        cur_point.SetActive(false);
+        NextStoryPoint();
     }
 }
