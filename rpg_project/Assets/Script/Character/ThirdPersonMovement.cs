fce9716982e226c4cedb8f9fa8f955c90c35fd74
Renaud Amedro <renaudamedro@gmail.com>
2021-04-16 18:00:52 +0200

    Ajout gravité

--- rpg_project/Assets/Script/Character/ThirdPersonMovement.cs
+++ rpg_project/Assets/Script/Character/ThirdPersonMovement.cs
@@ -14,7 +14,8 @@ public class ThirdPersonMovement : MonoBehaviour
     public float turn_smooth_time = 0.1f;
     private float horizontal, vertical;
     private Vector3 direction;
-    private float gravity_value =2500f;
+    private Vector3 move_dir;
+    private float gravity_value = 2500f;
     public void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -23,7 +24,7 @@ public class ThirdPersonMovement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            speed = 2*initial_speed;
+            speed = 2 * initial_speed;
         }
         else
             speed = initial_speed;
@@ -35,12 +36,16 @@ public class ThirdPersonMovement : MonoBehaviour
             float tgt_angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, tgt_angle, ref turn_smooth_velocity, turn_smooth_time);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            Vector3 move_dir = Quaternion.Euler(0f, tgt_angle, 0f) * Vector3.forward;
-            controller.Move(move_dir.normalized * speed * Time.deltaTime);
+            move_dir = Quaternion.Euler(0f, tgt_angle, 0f) * Vector3.forward;
             m_animator.SetBool("run", true);
         }
         else
             m_animator.SetBool("run", false);
+
+        //Ajout gravité
+        move_dir = new Vector3(move_dir.x, move_dir.y - 1f, move_dir.z);
+        //Move final du player
+        controller.Move(move_dir.normalized * speed * Time.deltaTime);
 
         if (Input.GetMouseButtonDown(0))
         {
